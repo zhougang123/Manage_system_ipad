f2b0986f595a71f823a50b4ba0604a895d9fba10
zhougang <zhougang@qfpay.com>
2015-09-25 09:53:55 +0800

    frist commit

---
+++ PGGame/PGGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PGGame
+//
+//  Created by mac on 15/9/25.
+//  Copyright (c) 2015年 qfpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
